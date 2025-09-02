b98cefcf5061392a7d4cda27178d021959363b8f
Rosvadle MANFO <florientmanfo3@outlook.fr>
2025-09-02 16:18:06 +0200

    Feat: Refactor routing process

--- src/app/feature/dashboard/dashboard.ts
+++ src/app/feature/dashboard/dashboard.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { 
+  }
 
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (!sessionStorage.getItem('token')) {
+        this.router.navigate(['/auth'], { replaceUrl: true });
+      }
+    }
+  }
 }
