b8b2980224e3f03d72906f0be6dce5ae3baf83bc
Rosvadle MANFO <florientmanfo3@outlook.fr>
2025-09-02 16:18:06 +0200

    Feat: Refactor routing process

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,12 @@ import { accountActivationGuard } from './core/account-activation-guard';
 import { Authentication } from './feature/authentication/authentication';
 import { Dashboard } from './feature/dashboard/dashboard';
 import { LandingPage } from './feature/landing-page/landing-page';
-import { App } from './app';
 
 export const routes: Routes = [
     {
         path: '',
-        component: App
+        redirectTo: 'landing',
+        pathMatch: 'full'
     },
     {
         path: 'auth',
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,11 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
 })
-export class App implements OnInit {
+export class App {
   protected title = 'cook';
-
-  constructor(
-    private router: Router,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) { }
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      const nextLink = localStorage.getItem('nextLink') ?? '/landing';
-      this.router.navigate([nextLink], { replaceUrl: true });
-    }
-  }
 }
