95e13d35da1b9c5248aa73c713e6d9aab4e4f321
Rosvadle MANFO <florientmanfo3@outlook.fr>
2025-10-31 08:38:10 +0100

    Feat: Use custom spinner instead of mat-spinner

--- src/app/feature/home/components/header/header.component.ts
+++ src/app/feature/home/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { StatsModel } from '../../../../core/model/admin/stats.model';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
+import { Spinner } from "../../../../utils/components/spinner/spinner";
 
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatProgressSpinnerModule, Spinner],
   templateUrl: './header.component.html',
 })
 export class Header {
