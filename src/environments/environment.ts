0820951d74a02691dcbf6e23b97afcd8402e0343
Rosvadle MANFO <florientmanfo3@outlook.fr>
2025-11-03 00:20:12 +0100

    Feat: Update environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: false,
+    apiUrl: '',
+    app: {
+        name: '',
+        version: '',
+        description: '',
+        tagline: '',
+    },
+    contact: {
+        email: '',
+    },
+    legal: {
+        privacyPolicyLastUpdate: '',
+        termsOfServiceLastUpdate: '',
+        copyrightYear: 2025,
+        companyName: '',
+    },
+};
